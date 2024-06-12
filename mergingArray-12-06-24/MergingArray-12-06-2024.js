b671941b186ee9f0cebcb561aa9e516d12a5e6ab
BABARIYADARSHIL <darshilbabariya444.com>
2024-06-12 15:18:13 +0530

    Change MargingArray-12-06-2024

--- mergingArray-12-06-24/MergingArray-12-06-2024.js
+++ mergingArray-12-06-24/MergingArray-12-06-2024.js
@@ -4,6 +4,6 @@ const mergingArray = (arr1, arr2) => {
     let arr3 = arr1.concat(arr2);
     let Duplicate = arr3.filter((item, index) => arr3.indexOf(item) === index)
     return Duplicate.sort();
-    }
-    console.log(mergingArray(arr1, arr2))
-   
+}
+console.log(mergingArray(arr1, arr2))
+
