49d0a1febbc761b75e1ed015005eb9c39a25c993
BABARIYADARSHIL <darshilbabariya444.com>
2024-06-12 15:15:03 +0530

    Chage MergingArray-12-06-2024

--- mergingArray-12-06-24/MergingArray-12-06-2024.js
+++ mergingArray-12-06-24/MergingArray-12-06-2024.js
@@ -1,8 +1,9 @@
 let arr1 = [2, 4, 3, 5, 4, 6]
 let arr2 = [1, 2, 6, 5, 4, 6]
-const mergingArray =(arr1,arr2)=>{
+const mergingArray = (arr1, arr2) => {
     let arr3 = arr1.concat(arr2);
     let Duplicate = arr3.filter((item, index) => arr3.indexOf(item) === index)
-    console.log(Duplicate.sort());
-}
-mergingArray(arr1,arr2)
+    return Duplicate.sort();
+    }
+    console.log(mergingArray(arr1, arr2))
+   
